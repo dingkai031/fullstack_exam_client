12f5af5e1e3b784d8960b8415d9140fb71c25798
Yovan Julio <yovanjulioadam@gmail.com>
2023-12-15 19:25:49 +0700

    test

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -11,6 +11,7 @@ export default () => {
           <Link to="/" className="text-blue-600 underline">
             Back to home
           </Link>
+          <p>{import.meta.env.VITE_TOP_LEVEL_DOMAIN}</p>
         </p>
       </div>
     </div>
